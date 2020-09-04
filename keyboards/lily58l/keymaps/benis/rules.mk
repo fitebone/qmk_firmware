9c57cfffaacbc15f5afc697e1c0bad1b72f1cf88
JJ <jjfoster7@protonmail.com>
2020-09-04 16:39:36 -0400

    Lily58l initial

---
+++ keyboards/lily58l/keymaps/benis/rules.mk
@@ -0,0 +1,9 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+RGB_MATRIX_ENABLE = WS2812
+
+ENCODER_ENABLE = no       # ENables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB light
+
+# uncomment to the line to customize the setting
+#define ENCODER_DIRECTION_FLIP # Switch encoder’s directions
+#define ENCODER_RESOLUTION 4 # Tune encoder resolution
--- keyboards/lily58l/rev1/rules.mk
+++ keyboards/lily58l/rev1/rules.mk
@@ -1,5 +1,5 @@
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+ENCODER_ENABLE = no       # ENables the use of one or more encoders
 # RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
 
-SRC += led_config.c
+SRC +=  led_config.c \
